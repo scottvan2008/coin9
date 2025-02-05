16016abf55f24d59a75f15836e9d915f0dfa66f2
Scott Yinan Fan <scottvan2008@gmail.com>
2025-02-06 03:23:19 +0800

    try to fix useTranslation

--- app/[lng]/components/Footer/index.tsx
+++ app/[lng]/components/Footer/index.tsx
@@ -1,25 +1,20 @@
 "use client"
 
 import { useEffect, useState } from 'react';
-import { useTranslation } from '../../../i18n';
+import { useTranslation } from '../../../i18n'; // Your custom useTranslation
 import { FooterBase } from './FooterBase';
 import { i18n as I18NextInstance } from 'i18next';
 
-
 export const Footer = ({ lng, path }: { lng: string; path?: string }) => {
-  // const [i18n, setI18n] = useState<any>(null); // State to store i18n data
   const [i18n, setI18n] = useState<I18NextInstance | null>(null);
 
-  
-  
-
   useEffect(() => {
-    const fetchTranslation = async () => {
-      const { i18n } = await useTranslation(lng, 'footer');
+    const loadTranslations = async () => {
+      const { i18n } = await useTranslation(lng, 'footer'); // Use the custom useTranslation
       setI18n(i18n);
     };
 
-    fetchTranslation();
+    loadTranslations();
   }, [lng]);
 
   if (!i18n) return null; // Prevent rendering until i18n is loaded
