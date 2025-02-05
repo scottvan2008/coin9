20abf0a37e5e7ef80b30aa0c42f13ae232ed7fcb
Scott Yinan Fan <scottvan2008@gmail.com>
2025-02-06 03:49:10 +0800

    restore index.tsx

--- app/[lng]/components/Footer/index.tsx
+++ app/[lng]/components/Footer/index.tsx
@@ -1,24 +1,11 @@
-"use client";
+import { useTranslation } from '../../../i18n'
+import { FooterBase } from './FooterBase'
 
-import { useEffect, useState } from 'react';
-import { useTranslation as useCustomTranslation } from '../../../i18n'; // Your custom useTranslation
-import { FooterBase } from './FooterBase';
-import { i18n as I18NextInstance } from 'i18next';
-
-export const Footer = ({ lng, path }: { lng: string; path?: string }) => {
-  const [i18n, setI18n] = useState<I18NextInstance | null>(null);
-
-  useEffect(() => {
-    // Handle the asynchronous loading of translations
-    const loadTranslations = async () => {
-      const { i18n } = await useCustomTranslation(lng, 'footer'); // Using custom useTranslation
-      setI18n(i18n);
-    };
-
-    loadTranslations();
-  }, [lng]); // Re-run effect when language changes
-
-  if (!i18n) return null; // Prevent rendering until i18n is loaded
-
-  return <FooterBase i18n={i18n} lng={lng} path={path} />;
-};
+export const Footer = async ({ lng, path }: {
+  lng: string;
+  path?: string;
+}) => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { t, i18n } = await useTranslation(lng, 'footer')
+  return <FooterBase i18n={i18n} lng={lng} path={path} />
+}
