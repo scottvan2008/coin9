de7d0c7bc1a8250390ccb87cbc4131d49e84a62b
Scott Yinan Fan <scottvan2008@gmail.com>
2025-02-06 02:35:20 +0800

    Try to fix index.tsx

--- app/[lng]/components/Footer/index.tsx
+++ app/[lng]/components/Footer/index.tsx
@@ -1,12 +1,22 @@
-import { useTranslation } from '../../../i18n'
-import { FooterBase } from './FooterBase'
+"use client"
 
-export const Footer = async ({ lng, path }: {
-  lng: string;
-  path?: string;
-}) => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  // const { t, i18n } = await useTranslation(lng, 'footer')
-  const { i18n } = await useTranslation(lng, 'footer')
-  return <FooterBase i18n={i18n} lng={lng} path={path} />
-}
+import { useEffect, useState } from 'react';
+import { useTranslation } from '../../../i18n';
+import { FooterBase } from './FooterBase';
+
+export const Footer = ({ lng, path }: { lng: string; path?: string }) => {
+  const [i18n, setI18n] = useState<any>(null); // State to store i18n data
+
+  useEffect(() => {
+    const fetchTranslation = async () => {
+      const { i18n } = await useTranslation(lng, 'footer');
+      setI18n(i18n);
+    };
+
+    fetchTranslation();
+  }, [lng]);
+
+  if (!i18n) return null; // Prevent rendering until i18n is loaded
+
+  return <FooterBase i18n={i18n} lng={lng} path={path} />;
+};
